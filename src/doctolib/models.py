3a3ede3329eb3572584176a4fb4f88691bd501e5
remi <rbrat@myges.fr>
2022-04-11 14:40:41 +0200

    Add models

--- src/doctolib/models.py
+++ src/doctolib/models.py
@@ -1,22 +1,36 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class Appointment(models.Model):
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    user_type = models.CharField(max_length=50)
+    geo_location = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.headline
+        return self.first_name + " " + self.last_name
 
-class Tickets(models.Model):
+    def get_type(self):
+        return self.type
 
-   def __str__(self):
-       return self.headline
+class Appointment(models.Model):
+    slot = models.ForeignKey(Slot)
+    user_patient = models.ForeignKey(User)
+    user_doctor = models.ForeignKey(User)
+    confirmed = models.BooleanField(default=False)
+    done = models.BooleanField(default=False)
 
-class Practitioner(User):
 
-   def __str__(self):
-       return self.headline
+class Slot(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    is_booked = models.BooleanField(default=False)
 
-class Patient(User):
+class Ticket(models.Model):
+   appointment = models.ForeignKey(Appointment)
+   user = models.ForeignKey(User)
+   description = models.TextField()
 
    def __str__(self):
        return self.headline
