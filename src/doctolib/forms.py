68d2a39d8c21fd838fd443d0444fdb5e67da701e
remi <rbrat@myges.fr>
2022-04-12 17:49:56 +0200

    encore du css

--- src/doctolib/forms.py
+++ src/doctolib/forms.py
@@ -36,12 +36,22 @@ class UserCreationForm(forms.ModelForm):
     password1 = forms.CharField(
         label=_("Password"),
         strip=False,
-        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        widget=forms.PasswordInput(attrs={
+            "autocomplete": "new-password",
+            'class': 'form-control',
+            'style': 'max-width: 300px;',
+            'placeholder': 'Password',
+        }),
         help_text=password_validation.password_validators_help_text_html(),
     )
     password2 = forms.CharField(
         label=_("Password confirmation"),
-        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        widget=forms.PasswordInput(attrs={
+            "autocomplete": "new-password",
+            'class': 'form-control',
+            'style': 'max-width: 300px;',
+            'placeholder': 'Password confirmation',
+        }),
         strip=False,
         help_text=_("Enter the same password as before, for verification."),
     )
@@ -49,6 +59,13 @@ class UserCreationForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ("username",)
+        widgets = {
+            'username': TextInput(attrs={
+                'class': 'form-control',
+                'style': 'max-width: 300px;',
+                'placeholder': 'Username',
+            }),
+        }
         field_classes = {"username": UsernameField}
 
     def __init__(self, *args, **kwargs):
