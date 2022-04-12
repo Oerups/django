7035f0365287c1f292d7836d0923b0237f19df66
tdargent1 <tdargent1@gmail.com>
2022-04-12 13:26:49 +0200

    admin panel

--- src/doctolib/admin.py
+++ src/doctolib/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from .models import Ticket
 from .models import User
 
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title',)
+    pass
+admin.site.register(Ticket, TicketAdmin)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff',)
+    pass
 admin.site.register(User, UserAdmin)
