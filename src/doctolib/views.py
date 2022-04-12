687a05293eb20236f106ace9bf469f0838ceaad8
LudovicCollignon <lud.collignon@gmail.com>
2022-04-12 11:17:31 +0200

    fix register with groups

--- src/doctolib/views.py
+++ src/doctolib/views.py
@@ -17,7 +17,7 @@ def register(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            group = Group.objects.get('patient')
+            group = Group.objects.get(name='patient')
             group.user_set.add(user.id)
             login(request, user)
             return redirect('index')
